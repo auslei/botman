b9e2cf525e02e8a0bf35fc22c6762f94577b7d3b
Anthony Sun <auslei@gmail.com>
2025-10-18 12:32:42 +1100

    Add .env file, sample agent, and update dependencies for FastMCP integration

---
+++ browserbot/sample_agent.py
@@ -0,0 +1,47 @@
+# agent.py
+import os
+import asyncio
+import mcp
+from openai import OpenAI
+from agents  import Agent, Runner, trace
+from agents.mcp import MCPServerStdio
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load .env from the project root (adjust path if your .env is elsewhere)
+dotenv_path = Path(__file__).resolve().parents[1] / ".env"
+load_dotenv(dotenv_path)  # loads into os.environ
+
+# 1) OpenAI client
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"], base_url="https://api.openai.com/v1")
+#client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'), base_url="https://api.deepseek.com")
+
+async def main():
+    async with MCPServerStdio(
+        name="Filesystem Server via npx",
+        params={
+            "command": "uv",
+            "args": [
+                "run",
+                "--project",
+                "G:\\dev\\botman",
+                "--with",
+                "fastmcp",
+                "fastmcp",
+                "run",
+                "G:\\dev\\botman\\browserbot\\fastmcp_server.py"
+            ],
+        }
+    ) as mcp_server:
+        agent = Agent(name="BrowserBot Agent",
+                      mcp_servers=[mcp_server],
+                      model="gpt-5-nano",
+                      handoff_description="Use the browser automation tools to interact with web pages as needed.")
+        with trace("browser workflow"):
+            result = await Runner.run(agent,"Browse the internet and find the linkedin page of Anthony Sun, he worked in the tech industry in Australia. Return the job titles listed on his profile.")
+            print(result.final_output)
+
+# Run the async function
+asyncio.run(main())
+
